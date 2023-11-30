c729daed915b06125b20a36c59a5a285744318c6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2023-11-30 13:52:33 +0700

    final

--- script.js
+++ script.js
@@ -7,17 +7,35 @@ const loadComment = () => {
   const personJSON = localStorage.getItem('comment')
   const person = JSON.parse(personJSON)
 
-  if (person) {
-    showComment.innerHTML = `
-            <p><b>${person.name}</b></p>
-            <p>${person.comment}</p>
-        `
-  } else {
-    showComment.innerHTML = '<p>There is no comment</p>'
+  if (!person) {
+    showComment.innerHTML = '<p>There are no comments</p>'
+
+    return
   }
+
+  let finalComment = ''
+
+  for (let i = 0; i < person.length; i++) {
+    const name = person[i].name
+    const comment = person[i].comment
+
+    finalComment =
+      finalComment +
+      `
+        <div class="comment">
+            <h2>${name}</h2>
+            <p>${comment}</p>
+        </div>
+    `
+  }
+
+  showComment.innerHTML = finalComment
 }
 
 buttonSend.addEventListener('click', () => {
+  const currentComment = localStorage.getItem('comment')
+  const currentCommentList = JSON.parse(currentComment)
+
   const inputNameValue = inputName.value
   const inputCommentValue = inputComment.value
 
@@ -26,11 +44,20 @@ buttonSend.addEventListener('click', () => {
     comment: inputCommentValue,
   }
 
-  const personJSON = JSON.stringify(person)
+  let listPerson = []
+
+  if (currentCommentList) {
+    listPerson = currentCommentList.concat(person)
+  } else {
+    listPerson.push(person)
+  }
+
+  const personJSON = JSON.stringify(listPerson)
 
   localStorage.setItem('comment', personJSON)
 
   inputComment.value = ''
+  inputName.value = ''
 
   loadComment()
 })
